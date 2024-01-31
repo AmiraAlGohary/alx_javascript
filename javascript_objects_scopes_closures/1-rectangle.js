30bf103da7418f6a0bc7663d2e0b957f2638e5f0
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-01-31 21:55:27 +0200

    my answer

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,3 @@
-
-// The constructor must take 2 arguments w and h
-// Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
-
 class Rectangle {
     constructor(w, h) {
         this.width = w;
