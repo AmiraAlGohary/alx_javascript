c0ff9c45124843f91f65cca154a9e1a10cdd88b0
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-01 00:41:42 +0200

    my answer

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+import { Rectangle } from "./4-rectangle.js";
+
+
+class Square extends Rectangle {
+    constructor(size){
+        super();
+        this.size = size;
+    }
+}
+
+module.exports = Square
