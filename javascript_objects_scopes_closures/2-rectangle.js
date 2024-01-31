25312a2757aeed3a20208d3c2fcda65c13cb108b
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-01-31 22:33:42 +0200

    update to my answer

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -16,25 +16,10 @@ class Rectangle {
     }    
 }
 
-// module.exports = Rectangle
-
-// class Rectangle {
-//     constructor(w, h) {
-//         this.width = undefined;
-//         this.height = undefined;
-
-//         if (w>0 && h>0 && Number.isInteger(w) && Number.isInteger(h)) {
-//             this.width = w;
-//             this.height = h;
-//         }
-//         else {
-//             return {};
-//         }
-//     }    
-// }
+module.exports = Rectangle
 
 
-const r1 = new Rectangle(3, -3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+// const r1 = new Rectangle(3, -3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
