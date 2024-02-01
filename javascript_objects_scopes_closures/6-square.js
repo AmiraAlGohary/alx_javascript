1c5edd4e0ccadf9229ef0607f0971116de243201
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-01 03:46:07 +0200

    update to my answer

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,13 +4,12 @@ const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
     constructor(size){
         super(size, size);
-        this.size = size;
     }
     
     charPrint(c) {
         if (typeof c === 'undefined') {
-            for (let i=0; i <this.size; i++) {
-                for (let j=0; j <this.size; j++) {
+            for (let i=0; i <this.height; i++) {
+                for (let j=0; j <this.width; j++) {
                     process.stdout.write('X');
                 }            
                 process.stdout.write('\n');
@@ -18,21 +17,15 @@ class Square extends Rectangle {
         }
         
         else {
-            for (let i=0; i <this.size; i++) {
-                for (let j=0; j <this.size; j++) {
+            for (let i=0; i <this.height; i++) {
+                for (let j=0; j <this.width; j++) {
                     process.stdout.write(c);
                 }            
                 process.stdout.write('\n');
             }
         }
     }
+
 }
-
-
-
-const s1 = new Square(3);
-s1.charPrint('D');
-s1.double();
-s1.charPrint('O');
 
 module.exports = Square;
