32e1dec2a146b4144ab44ea57e85a01cad1c112d
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 01:28:15 +0200

    updates

--- javascript-network/trial.js
+++ javascript-network/trial.js
@@ -1,1 +1,11 @@
-const response = fetch('https://dog.ceo/api/breeds/image/random')
+// const response = fetch('https://dog.ceo/api/breeds/image/random')
+
+const fetch = require('node-fetch');
+url = 'http://worldtimeapi.org/api/timezone/America/New_York'
+
+async function getData() {
+    const response = await fetch(url)
+    console.log(response)
+}
+
+getData()
