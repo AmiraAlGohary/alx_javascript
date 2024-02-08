cfb22fcfd303d91d2c02becbeb5c00aec4001f38
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 23:20:24 +0200

    my answer

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const url = process.argv[2];
+
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('error:', error)
+        return;
+    }
+    
+    const todos = JSON.parse(body);
+    const completedTasksByUserId = {};
+
+    todos.forEach(todo => {
+        if (todo.completed) {
+            const userId = todo.userId;
+            completedTasksByUserId[userId] = (completedTasksByUserId[userId] || 0) + 1;
+        }
+    });
+
+    Object.keys(completedTasksByUserId).forEach(userId => {
+        console.log(`${userId}: ${completedTasksByUserId[userId]}`);
+    });
+});
