1da9569a32d6b6a910dd142d64248735adf3bc52
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 21:38:21 +0200

    my answer

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,17 @@
+const requestStar = require('request');
+
+const url4 = 'https://swapi-api.alx-tools.com/api/films/:id';
+
+const movieID = process.argv[2];
+
+requestStar.get(url4, (error, response, body) => {
+    if (error) {
+        console.error('error:', error)
+        return;
+    }
+    
+    const data = Json.parse(body);
+    const dataMovieName = data.title;
+    console.log(dataMovieName);
+}
+);
--- javascript-network/trial.js
+++ javascript-network/trial.js
@@ -1,11 +1,67 @@
 // const response = fetch('https://dog.ceo/api/breeds/image/random')
 
-const fetch = require('node-fetch');
-url = 'http://worldtimeapi.org/api/timezone/America/New_York'
+// import fetch from 'node-fetch';
+
+const url = 'http://worldtimeapi.org/api/timezone/America/New_York'
 
 async function getData() {
-    const response = await fetch(url)
-    console.log(response)
+    try {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 getData()
+// -----------------------
+
+const url2 = 'http://worldtimeapi.org/api/timezone/America/New_York'
+
+async function get_Data() {
+    try {
+        const request = new Request(url2, {
+            headers: {
+                'Authorization': 'something'
+            }
+        })
+        const resp = await fetch(request);
+        const data = await resp.json();
+        console.log(data)
+    } catch(error) {
+        console.error('Error:', error);
+    }
+}
+
+// -----------------------
+
+const url3 = 'http://worldtimeapi.org/api/timezone/America/New_York'
+
+const request = new Request(url3, {
+    headers: {
+        'Authorization': 'something'
+    }
+})
+
+fetch(request)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
