7fab7efe2f89f2c8ee85f260f6929ccb6bd76bdc
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 23:36:53 +0200

    update to my answer

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -9,8 +9,20 @@ request.get(url, (error, response, body) => {
         return;
     }
     
-    const data = JSON.parse(body);
-    const MovieCharacters = data.characters;
-    console.log(MovieCharacters);
-}
-);
+    const film = JSON.parse(body);
+    // const MovieCharacters = film.characters;
+    // console.log(MovieCharacters);
+
+    film.characters.forEach(characterUrl => {
+        request.get(characterUrl, (charError, charResponse, charBody) => {
+            if (charError) {
+                console.error('Error:', charError);
+            } else {
+                const character = JSON.parse(charBody);
+                console.log(character.name);
+            }
+        });
+    });
+
+
+});
