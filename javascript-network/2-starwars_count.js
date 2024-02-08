33aea94d1b1e1786027c2bd203a56be5dd26e319
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 22:31:42 +0200

    update to my answer

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,18 +1,17 @@
 const requestStar = require('request');
 
-// const url5 = 'https://swapi-api.alx-tools.com/api/films/'
 const url5 = process.argv[2];
+const characterId = 18;
 
-requestStar.get('https://swapi-api.alx-tools.com/api/people/18', (error, response, body) => {
+requestStar.get(url5, (error, response, body) => {
     if (error) {
         console.error('error:', error)
         return;
     }
     
     const data = JSON.parse(body);
-    const theMovies = data.films;
-    const noOfMovies = theMovies.length
+    const moviesWithWedge = data.results.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+    const noOfMovies = moviesWithWedge.length
     console.log(noOfMovies);
-    
 }
 );
