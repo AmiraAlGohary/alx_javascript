428f21ae740136900550cd02021cd1b37cdce4c9
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-08 21:40:40 +0200

    update to my answer

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,6 +1,6 @@
 const requestStar = require('request');
 
-const url4 = 'https://swapi-api.alx-tools.com/api/films/:id';
+const url4 = 'https://swapi-api.alx-tools.com/api/films/${movieID}/';
 
 const movieID = process.argv[2];
 
