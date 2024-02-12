8492610a6718b2fac9407b5af679284f9ada9276
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-12 20:03:38 +0200

    my answer

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,19 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        }
+    };
+
+    let students = [];
+    for (let i =0; i< numbersOfStudents; i++) {
+        students.push(studentSeat(i+1)());
+    };
+    
+    return students;
+
+};
+
+function classRoom() {
+    return createClassRoom(10);
+};
