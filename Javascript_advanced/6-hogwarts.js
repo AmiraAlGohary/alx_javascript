7c5f52bbd01a112d98deb9034436eac82770be55
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-13 04:25:45 +0200

    files created

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,19 @@
+// Write a module named studentHogwarts:
+    // It contains two variables privateScore set to 0, and name set to null
+    // It contains one private method changeScoreBy, it takes points in argument and add it to privateScore
+    // The module gives access to four public methods (return an object):
+        // setName, it takes into argument newName, and set the private variable name
+        // rewardStudent, it calls the method changeScoreBy with 1
+        // penalizeStudent, it calls the method changeScoreBy with -1
+        // getScore, it returns name: score (ex: Harry: 14)
+// ------------------------------
+// Create one variable named harry, that is an instance of studentHogwarts:
+    // Set the name of the object to Harry
+    // Reward the student four times
+    // Log to the console the name and score
+// ------------------------------
+// Create one variable named draco, that is an instance of studentHogwarts:
+    // Sets the name of the object to Draco
+    // Reward the student one time
+    // Penalize the student three times
+    // Log to the console the name and score
