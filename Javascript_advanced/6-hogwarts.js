d074c04d6df78f6cba6dd4e4e4c01365621a2d35
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-13 22:02:39 +0200

    my answer

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,19 +1,47 @@
-// Write a module named studentHogwarts:
-    // It contains two variables privateScore set to 0, and name set to null
-    // It contains one private method changeScoreBy, it takes points in argument and add it to privateScore
-    // The module gives access to four public methods (return an object):
-        // setName, it takes into argument newName, and set the private variable name
-        // rewardStudent, it calls the method changeScoreBy with 1
-        // penalizeStudent, it calls the method changeScoreBy with -1
-        // getScore, it returns name: score (ex: Harry: 14)
-// ------------------------------
 // Create one variable named harry, that is an instance of studentHogwarts:
     // Set the name of the object to Harry
     // Reward the student four times
     // Log to the console the name and score
-// ------------------------------
+
 // Create one variable named draco, that is an instance of studentHogwarts:
     // Sets the name of the object to Draco
     // Reward the student one time
     // Penalize the student three times
     // Log to the console the name and score
+
+let privateScore = 0;
+let name = null;
+
+function changeScoreBy(points) {
+    privateScore += points;
+};
+
+const studentHogwarts = {
+    setName: function(newName) {
+        name = newName;
+    },
+    rewardStudent: function() {
+        changeScoreBy(1);
+    },
+    penalizeStudent: function() {
+        changeScoreBy(-1);
+    },
+    getScore: function() {
+        return `${name}: ${privateScore}`;
+    }
+};
+
+let harry = Object.create(studentHogwarts);
+harry.setName('Harry');
+for(let i=0; i <4; i++) {
+    harry.rewardStudent();
+};
+console.log(harry.getScore());
+
+let draco = Object.create(studentHogwarts);
+draco.setName('Draco');
+draco.rewardStudent();
+for(let i=0; i <3; i++) {
+    draco.penalizeStudent();
+};
+console.log(draco.getScore());
