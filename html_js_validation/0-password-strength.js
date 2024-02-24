04fae7832398402de895c6ede89465d73aecaa8e
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-02-24 03:31:08 +0200

    update to my answer

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,13 +1,39 @@
 const passwordError = document.getElementById('password-error');
-const submitError = document.getElementById('submit-error');
+const submitError = document.getElementById('error');
 
 function validatePassword() {
     const password = document.getElementById('password').value;
+    passwordError.innerHTML = '';
 
     if(password.length < 8) {
         passwordError.innerHTML = 'Password must be 8 characters or more.';
         return false;
     }
+
+    // Check for at least one uppercase letter
+    if (!/[A-Z]/.test(password)) {
+        passwordError.innerHTML = "Password must contain at least one uppercase letter.";
+        return false;
+    }
+
+    // Check for at least one lowercase letter
+    if (!/[a-z]/.test(password)) {
+        passwordError.innerHTML = "Password must contain at least one lowercase letter.";
+        return false;
+    }
+
+    // Check for at least one numeric digit
+    if (!/\d/.test(password)) {
+        passwordError.innerHTML = "Password must contain at least one numeric digit.";
+        return false;
+    }
+
+    // Check for at least one special character
+    if (!/[!@#$%^&*]/.test(password)) {
+        passwordError.innerHTML = "Password must contain at least one special character.";
+        return false;
+    }
+
     passwordError.innerHTML = 'valid';
     return true;
 };
